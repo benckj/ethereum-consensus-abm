945888e254d44fa1e51f6aee08a1000732aeae56
Nicolo <nicolo.vallarano@imtlucca.it>
2022-12-02 16:57:22 +0000

    removed experiments

--- experiments/plot_all.sh
+++
@@ -1,2 +0,0 @@
-# a.csv: ER n=64, <d>=3
-python3 plot.py --input a.csv --x tau_block --y mainchain_rate --z tau_attestation --output tmp.pd
--- experiments/run1.sh
+++
@@ -1,2 +0,0 @@
-# ER, 300s tau_block 0.5/10, tau_attest 0.5/10
-python3 ../ethereum_abm.py --repeat=20 --workers=100 a1.spg
