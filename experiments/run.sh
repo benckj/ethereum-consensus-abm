861836e51e2849e4dd14b45693274ad2aa08cc04
Nicolo <nicolo.vallarano@imtlucca.it>
2022-12-02 09:13:22 +0000

    cleaning

--- experiments/run.sh
+++
@@ -1,6 +0,0 @@
-# ER, 300s tau_block 0.5/10, tau_attest 0.5/10
-python3 ../ethereum_abm.py --repeat=20 --workers=32 a.spg
-# UNIFORM, 300s tau_block 0.5/10, tau_attest 0.5/10
-python3 ../ethereum_abm.py --repeat=20 --workers=32 b.spg
-# BA, 300s tau_block 0.5/10, tau_attest 0.5/10
-python3 ../ethereum_abm.py --repeat=20 --workers=32 c.spg
